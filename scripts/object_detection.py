d9c02c4e75bff0e89b54bad5f4de617eb99793ab
EC2 Default User <ec2-user@ip-172-31-35-242.ap-northeast-2.compute.internal>
2024-10-23 05:36:16 +0000

    Set up a scalable Flask application using Nginx and Gunicorn for deployment

---
+++ scripts/object_detection.py
@@ -0,0 +1,18 @@
+import cv2
+from flask import Response
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
