8925ea73fc875d452ee215123115811bbbaf94af
jstar <seghuo22@gmail.com>
2024-11-22 21:22:09 +0900

    backup routes

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/product', require('./routes/product'));
+app.use('/api/book', require('./routes/book'));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
@@ -34,16 +35,15 @@ app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  
+
   // Set static folder
   app.use(express.static("client/build"));
-  
+
   // index.html for all page routes
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
-app.use('/api/book', require('./routes/book'));
 
 const port = process.env.PORT || 5000
 
