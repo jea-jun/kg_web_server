f3b61533b8651adfcc1bd58829132b1bc8067f07
jstar <seghuo22@gmail.com>
2024-11-22 22:00:28 +0900

    Fix: Removed option parameter from API request

--- server/routes/book.js
+++ server/routes/book.js
@@ -8,7 +8,7 @@ router.get('/getBooks', async (req, res) => {
   const url = "http://apis.data.go.kr/9720000/searchservice/basic"; // API URL
   
   // 클라이언트에서 전달된 쿼리 파라미터 추출
-  const { option, pageno, displaylines, search } = req.query;
+  const { pageno, displaylines, search } = req.query;
 
   // 요청 파라미터 객체 생성
   const params = {
@@ -16,7 +16,6 @@ router.get('/getBooks', async (req, res) => {
   };
 
   // 클라이언트 요청에 따라 동적으로 추가
-  if (option) params.option = option;          // 검색 옵션
   if (pageno) params.pageno = pageno;          // 페이지 번호
   if (displaylines) params.displaylines = displaylines; // 페이지당 항목 수
   if (search) params.search = search;          // 검색어
