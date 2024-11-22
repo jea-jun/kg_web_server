cd606e129f7f161e84953c962f97f3dc7abfdfc7
jstar <seghuo22@gmail.com>
2024-11-23 06:57:56 +0900

    Fix missing state updates in ReservationButton.js

--- client/src/components/views/LandingPage/Sections/ReservationButton.js
+++ client/src/components/views/LandingPage/Sections/ReservationButton.js
@@ -12,24 +12,9 @@ const handleDateChange = (e, selectedDateTime, setSelectedDateTime) => {
 const handleTimeChange = (e, selectedDateTime, setSelectedDateTime) => {
     setSelectedDateTime({ ...selectedDateTime, time: e.target.value });
 };
-
-const handleReservation = (selectedDateTime, setIsTimePickerVisible) => {
-    alert(`Reservation confirmed for ${selectedDateTime.date} at ${selectedDateTime.time}`);
-    setIsTimePickerVisible(false);
-    setSelectedDateTime({ date: '', time: '' }); // 선택된 날짜와 시간을 초기화
-};
-
-// 외부 클릭 감지 핸들러
-const handleOutsideClick = (e) => {
-    if (!e.target.closest('.card')) {
-        setSelectedCard(null); // 외부를 클릭하면 선택 해제
-    }
-};
     
 export {
     handleCardClick,
     handleDateChange,
     handleTimeChange,
-    handleReservation,
-    handleOutsideClick
 };
