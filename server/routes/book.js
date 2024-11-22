12926c8d4db23872f50ba0ba76d14cce6d9031d6
jstar <seghuo22@gmail.com>
2024-11-22 20:02:23 +0900

    Dynamic modification of an api call

--- server/routes/book.js
+++ server/routes/book.js
@@ -3,16 +3,22 @@ const router = express.Router();
 const axios = require("axios");
 const config = require("../config/key");
 
-
 // API 요청 라우트
 router.get('/getBooks', async (req, res) => {
   const url = "http://apis.data.go.kr/9720000/searchservice/basic"; // API URL
+
+  // 클라이언트에서 전달된 쿼리 파라미터 추출
+  const { pageno, displaylines, search } = req.query;
+
+  // 요청 파라미터 객체 생성
   const params = {
     serviceKey: config.libraryApiKey, // URL 인코딩된 인증키
-    pageno: req.query.pageno || 1,              // 페이지 번호 (기본값: 1)
-    displaylines: req.query.displaylines || 10, // 페이지당 항목 수 (기본값: 10)
-    search: req.query.search || "자료명,미래의 설계" // 검색어 (기본값: "미래의 설계")
   };
+
+  // 클라이언트 요청에 따라 동적으로 추가
+  if (pageno) params.pageno = pageno;          // 페이지 번호
+  if (displaylines) params.displaylines = displaylines; // 페이지당 항목 수
+  if (search) params.search = search;         // 검색어
 
   try {
     // GET 요청 전송
@@ -32,3 +38,4 @@ router.get('/getBooks', async (req, res) => {
 });
 
 module.exports = router;
+
