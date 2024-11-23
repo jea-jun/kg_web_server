9b66116c513c716185468365dd3a39d5bae8ed5b
jstar <seghuo22@gmail.com>
2024-11-24 05:31:46 +0900

    refactor: Simplify LibraryPage component by removing unnecessary elements and keeping essential content onl.

--- client/src/components/views/LibraryPage/LibraryPage.js
+++ client/src/components/views/LibraryPage/LibraryPage.js
@@ -1,31 +1,16 @@
 import React from 'react';
+import './Sections/LibraryPage.css';  // CSS 파일 직접 불러오기
 
 function LibraryPage() {
-    React.useEffect(() => {
-        document.title = "Library Page";
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = "./Sections/LibraryPage.css";
-        document.head.appendChild(link);
-
-        return () => {
-            document.head.removeChild(link); // Cleanup
-        };
-    }, []);
-
-    return (
-        <div>
-            <pre>
-                <code id="codeBlock">
-                    <div className="lineOfCode"><em>1200</em> Followers</div>
-                    <div className="lineOfCode">
-                        <strong>on</strong> <b>Threads</b> <u>🙏</u>
-                    </div>
-                    <div className="lineOfCode"><s>// Thank you !</s></div>
-                </code>
-            </pre>
-        </div>
-    );
+  return (
+    <div>
+      <div className="lineOfCode"><em>1200</em> Followers</div>
+      <div className="lineOfCode">
+        <strong>on</strong> <b>Threads</b> <u>🙏</u>
+      </div>
+      <div className="lineOfCode"><s>// Thank you !</s></div>
+    </div>
+  );
 }
 
 export default LibraryPage;
